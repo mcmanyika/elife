71ee132b5a6fa48fbde8a07e8802e2e4679e5940
Micah <developer@heartfeltonline.org>
2024-05-21 23:53:43 -0500

    added env

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,14 +4,14 @@ import { getDatabase } from "firebase/database";
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDBX-mSoWLV523ZHzAy4oSgWK_iu5P72ns",
-  authDomain: "insurance-1573c.firebaseapp.com",
-  databaseUrl: "https://insurance-1573c-default-rtdb.firebaseio.com/",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: "insurance-1573c",
-  storageBucket: "insurance-1573c.appspot.com",
-  messagingSenderId: "947234088295",
-  appId: "1:947234088295:web:bd4361db2b9c3b09707bc2",
-  measurementId: "G-V7MY4Z0BRR"
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
