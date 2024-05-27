f43b461f53aad67782a7fe925c0f2be3f542f884
Micah <developer@heartfeltonline.org>
2024-05-26 21:45:30 -0500

    update

--- src/app/components/Ad.js
+++ src/app/components/Ad.js
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function Ad() {
   return (
     <div className="flex justify-center p-5">
-        <div className="relative w-[360px] h-[60px] md:w-[650px] md:h-[100px]">
+        <div className="relative w-[300px] h-[60px] md:w-[650px] md:h-[100px]">
             <Image 
                 src="/images/b1.png" 
                 alt="Person" 
